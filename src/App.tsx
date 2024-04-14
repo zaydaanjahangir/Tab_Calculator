37ded6b7fbb3d8cda2ef0b3f0bdc0844de60c3cb
Anish Sahoo <anishsahoo2005@gmail.com>
2024-04-14 16:44:03 -0700

    Add a lot of code

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,12 @@ function App() {
   const [person, setPerson] = useState<Person[]>([]);
   const [addName, setAddName] = useState<string>("");
   const [selectedPerson, setSelectedPerson] = useState<number>(-1);
+  const [price, setPrice] = useState<number>(0);
+  const [setOfUsers, setSetOfUsers] = useState<Set<string>>(new Set<string>());
 
   return (
     <>
-      <h1 className="text-center font-mono text-4xl">Tab Calculator</h1>
+      <h1 className="text-center font-mono text-4xl mt-4">Tab Calculator</h1>
       <div className="mx-8 text-center mt-8 flex flex-col items-center justify-center">
         <div className="flex flex-row">
           <Textarea
@@ -32,7 +34,12 @@ function App() {
           />
           <Button
             className="ml-2"
-            onClick={() => setPerson([...person, { name: addName, items: [] }])}
+            onClick={() => {
+              if (setOfUsers.has(addName.toLowerCase())) return;
+              setPerson([...person, { name: addName, items: [] }]);
+              setSetOfUsers(setOfUsers.add(addName.toLowerCase()));
+              setAddName("");
+            }}
           >
             Add Person
           </Button>
@@ -42,29 +49,103 @@ function App() {
             <Card key={i} className="mt-2 px-4 py-2">
               <div className="flex flex-row">
                 <h2 className="text-xl font-bold my-auto mr-2">{p.name}</h2>
-                <Button className="text-lg" isIconOnly>
+                <Button
+                  className="text-lg"
+                  isIconOnly
+                  onClick={() => {
+                    setPerson(person.filter((_, index) => index !== i));
+                  }}
+                >
                   X
                 </Button>
+              </div>
+              <div>
+                {p.items.map((item, index) => (
+                  <div key={index} className="flex flex-row">
+                    <h3 className="text-sm font-semibold text-green-600">{`+ $${item}`}</h3>
+                    <Button
+                      className="text-sm h-6"
+                      isIconOnly
+                      onClick={() => {
+                        const newPerson = [...person];
+                        newPerson[i].items = newPerson[i].items.filter(
+                          (_, j) => j !== index,
+                        );
+                        setPerson(newPerson);
+                      }}
+                    >
+                      x
+                    </Button>
+                  </div>
+                ))}
               </div>
             </Card>
           ))}
         </div>
         {person.length > 0 && (
-          <Autocomplete
-            label="Select an Person"
-            className="max-w-xs"
-            selectedKey={selectedPerson}
-            onSelectionChange={(key: Key) =>
-              setSelectedPerson((key as number) || -1)
-            }
-          >
-            {person.map((person, index) => (
-              <AutocompleteItem key={index} value={person.name}>
-                {person.name}
-              </AutocompleteItem>
-            ))}
-          </Autocomplete>
+          <Card className="p-8 m-8 w-full items-center">
+            <div className="flex md:flex-row flex-col">
+              <Autocomplete
+                label="Select an Person"
+                className="max-w-xs mx-auto"
+                selectedKey={selectedPerson}
+                onSelectionChange={(key: Key) =>
+                  setSelectedPerson((key as number) || -1)
+                }
+              >
+                {person.map((person, index) => (
+                  <AutocompleteItem key={index} value={person.name}>
+                    {person.name}
+                  </AutocompleteItem>
+                ))}
+              </Autocomplete>
+              {selectedPerson !== -1 && (
+                <div className="flex flex-row mx-auto my-auto mt-2 sm:my-auto sm:ml-2">
+                  <Textarea
+                    placeholder="Price"
+                    className="max-w-xs mx-auto"
+                    maxRows={1}
+                    size="md"
+                    onChange={(e) => setPrice(Number(e.target.value))}
+                  />
+                  <Button
+                    className="ml-2"
+                    onClick={() => {
+                      if (price === 0) return;
+                      const newPerson = [...person];
+                      newPerson[selectedPerson].items.push(price as number);
+                      setPerson(newPerson);
+                      setPrice(0);
+                      console.log(person);
+                    }}
+                  >
+                    Add Price
+                  </Button>
+                </div>
+              )}
+            </div>
+          </Card>
         )}
+        <Card className="p-8 mx-8 w-full items-center">
+          <div className="flex flex-col sm:flex-row sm:gap-4 my-1 sm:my-0 sm:mb-4">
+            <Textarea
+              label="Fees/Tips"
+              className="max-w-xs mx-auto"
+              maxRows={1}
+              size="md"
+            />
+            <Textarea
+              label="Tax %"
+              className="max-w-xs mx-auto"
+              maxRows={1}
+              size="md"
+            />
+          </div>
+
+          <Button className="w-full" onClick={() => {}}>
+            Calculate
+          </Button>
+        </Card>
       </div>
     </>
   );
