f54e2d642e222070211012dbcbffb6af4082d63f
Anish Sahoo <anishsahoo2005@gmail.com>
2024-04-14 14:40:50 -0700

    Add next-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@nextui-org/react'
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1 className='text-center font-mono text-4xl'>Tab Calculator</h1>
+      <Button>NextUIButton</Button>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
-  plugins: [],
-}
-
+  darkMode: "class",
+  plugins: [nextui()],
+};
