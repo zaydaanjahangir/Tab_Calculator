8e67e5c5a0fd7c13a6e3553e13250f56ce973742
Anish Sahoo <anishsahoo2005@gmail.com>
2024-04-14 17:37:21 -0700

    Add a lot of stuff

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,13 @@ function App() {
   const [selectedPerson, setSelectedPerson] = useState<number>(-1);
   const [price, setPrice] = useState<number>(0);
   const [setOfUsers, setSetOfUsers] = useState<Set<string>>(new Set<string>());
+  const [feesTipes, setFeesTips] = useState<number>(0);
+  const [tax, setTax] = useState<number>(0);
 
   return (
     <>
       <h1 className="text-center font-mono text-4xl mt-4">Tab Calculator</h1>
-      <div className="mx-8 text-center mt-8 flex flex-col items-center justify-center">
+      <div className="mx-8 md:mx-32 text-center mt-8 flex flex-col items-center justify-center">
         <div className="flex flex-row">
           <Textarea
             placeholder="Name"
@@ -31,6 +33,7 @@ function App() {
             maxRows={1}
             size="md"
             onChange={(e) => setAddName(e.target.value)}
+            value={addName}
           />
           <Button
             className="ml-2"
@@ -40,32 +43,30 @@ function App() {
               setSetOfUsers(setOfUsers.add(addName.toLowerCase()));
               setAddName("");
             }}
-          >
+            >
             Add Person
-          </Button>
-        </div>
-        <div className="flex flex-row flex-wrap gap-1 my-4 mb-8">
-          {person.map((p, i) => (
+            </Button>
+          </div>
+          <div className="flex flex-row flex-wrap gap-2 my-4 mb-8">
+            {person.map((p, i) => (
             <Card key={i} className="mt-2 px-4 py-2">
-              <div className="flex flex-row">
-                <h2 className="text-xl font-bold my-auto mr-2">{p.name}</h2>
-                <Button
-                  className="text-lg"
-                  isIconOnly
-                  onClick={() => {
+              <div className="flex flex-row justify-between">
+              <h2 className="text-xl font-bold my-auto mr-2">{p.name}</h2>
+              <button
+                className="text-sm self-start text-red-500"
+                onClick={() => {
                     setPerson(person.filter((_, index) => index !== i));
                   }}
                 >
                   X
-                </Button>
+                </button>
               </div>
               <div>
                 {p.items.map((item, index) => (
-                  <div key={index} className="flex flex-row">
-                    <h3 className="text-sm font-semibold text-green-600">{`+ $${item}`}</h3>
-                    <Button
-                      className="text-sm h-6"
-                      isIconOnly
+                  <div key={index} className="flex flex-row hover:bg-red-200">
+                    <h3 className="text-sm font-semibold text-green-600 pt-1 mr-2 w-full text-left">{`+ $${item}`}</h3>
+                    <button
+                      className="self-start"
                       onClick={() => {
                         const newPerson = [...person];
                         newPerson[i].items = newPerson[i].items.filter(
@@ -75,7 +76,7 @@ function App() {
                       }}
                     >
                       x
-                    </Button>
+                    </button>
                   </div>
                 ))}
               </div>
@@ -107,6 +108,7 @@ function App() {
                     maxRows={1}
                     size="md"
                     onChange={(e) => setPrice(Number(e.target.value))}
+                    value={price === 0 ? "" : price.toString()}
                   />
                   <Button
                     className="ml-2"
@@ -116,7 +118,6 @@ function App() {
                       newPerson[selectedPerson].items.push(price as number);
                       setPerson(newPerson);
                       setPrice(0);
-                      console.log(person);
                     }}
                   >
                     Add Price
@@ -126,26 +127,38 @@ function App() {
             </div>
           </Card>
         )}
-        <Card className="p-8 mx-8 w-full items-center">
-          <div className="flex flex-col sm:flex-row sm:gap-4 my-1 sm:my-0 sm:mb-4">
+        <Card className="p-8 mx-8 mb-8 w-full items-center">
+          <div className="flex flex-col sm:flex-row sm:gap-4 my-1 sm:my-0 sm:mb-4 gap-2">
             <Textarea
               label="Fees/Tips"
               className="max-w-xs mx-auto"
               maxRows={1}
               size="md"
+              onChange={(e) => 
+                setFeesTips(Number(e.target.value))
+              }
             />
             <Textarea
               label="Tax %"
               className="max-w-xs mx-auto"
               maxRows={1}
               size="md"
+              onChange={(e) =>
+                setTax(Number(e.target.value))
+              }
             />
           </div>
-
-          <Button className="w-full" onClick={() => {}}>
-            Calculate
-          </Button>
+          <div className="flex flex-row flex-wrap">
+              {person.map((p, i) => (
+                <Card key={i} className="m-2 p-4">
+                  <h1>{p.name}</h1>
+                  <h2></h2>
+                  <h2>${((p.items.reduce((total, item) => total + item, 0) * (1 + tax / 100)) + (feesTipes/person.length)).toFixed(2)}</h2>
+                </Card>
+              ))}
+          </div>
         </Card>
+        <h1 className="text-gray-400 text-sm text-center text-wrap">Made by Anish Sahoo, Zaydaan Jahangir, William Riser</h1>
       </div>
     </>
   );
